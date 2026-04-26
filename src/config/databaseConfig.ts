467248aa76662141b4d1be70988a9cc147e87b6d
Femi Abejide <abejidefemi1@gmail.com>
2026-04-26 23:52:32 +0200

    update config

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -2,14 +2,13 @@ import mongoose from "mongoose";
 
 export default (async function dbConfig() {
   try {
-    console.log(process.env.NODE_ENV);
     if (process.env.NODE_ENV === "development") {
       await mongoose.connect("mongodb://localhost:27017/customer-support");
       console.log("connected to development database");
       return;
     } else {
       await mongoose.connect(
-        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pgmrh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
+        `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.pgowv0z.mongodb.net/?appName=${process.env.APP_NAME}`
       );
       console.log("connected to production database");
       return;
